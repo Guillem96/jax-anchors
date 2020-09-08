d913829eaee637e5a0137f98517ea1e14da7b459
Guillem96 <guillem.orellana@gmail.com>
2020-09-08 07:54:34 +0200

    np.where seems unstable for dereivatives

--- anchors_jax/nn/losses.py
+++ anchors_jax/nn/losses.py
@@ -14,27 +14,31 @@ def sparse_cross_entropy(y_true: Tensor, y_pred: Tensor) -> Tensor:
     >>> predictions = jax.nn.softmax(jax.random.uniform(key, shape=(10, 3)))
     >>> loss = aj.nn.losses.sparse_cross_entropy(target, predictions)
     """
-    epsilon = 1e-6
+    epsilon = 1e-8
     y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     y_true = jax.nn.one_hot(y_true, y_pred.shape[-1])
     return y_true * -np.log(y_pred)
 
 
 def binary_cross_entropy(y_true: Tensor, y_pred: Tensor) -> Tensor:
-    epsilon = 1e-6
+    epsilon = 1e-8
     y_pred = y_pred.reshape(-1)
-    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-
     y_true = y_true.astype('float32')
 
-    pt = np.where(y_true == 1, y_pred, 1 - y_pred)
-    return -np.log(pt)
+    loss = y_true * np.log(y_pred + epsilon)
+    loss += (1. - y_true) * np.log(1. - y_pred + epsilon)
+    return -loss
 
 
 def smooth_l1(y_true: Tensor, y_pred: Tensor) -> Tensor:
+    y_true = y_true.reshape(-1, 4).astype('float32')
+    y_pred = y_pred.reshape(-1, 4).astype('float32')
+
     error = y_true - y_pred
     abs_error = np.abs(error)
+    mask = (abs_error < 1.).astype('float32')
 
-    return np.where(abs_error < 1., 
-                    np.power(error, 2) * .5,
-                    abs_error - .5)
+    quad_error = np.power(error, 2) * .5
+    lin_error = abs_error - .5
+
+    return mask * quad_error + (1. - mask) * lin_error
